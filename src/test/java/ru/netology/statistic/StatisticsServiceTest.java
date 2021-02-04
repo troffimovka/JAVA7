07562d85dae192c647b89b5a441b80e931eef06b
troffimovka <troffimovka@gmail.com>
2021-02-05 00:42:26 +0300

    test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,5 +17,15 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxMonthlyIncomeAfterFeb() {
+        StatisticsService service = new StatisticsService();
 
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
